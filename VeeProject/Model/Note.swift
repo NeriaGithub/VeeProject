1cf05d4a41bd7e6b04faeb540444b9e18bc78e9d
NeriaGithub <neria,jerafi@gmail.com>
2020-09-03 20:28:00 +0300

    create Note struct

---
+++ VeeProject/Model/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  VeeProject
+//
+//  Created by Neria Jerafi on 03/09/2020.
+//  Copyright © 2020 Neria Jerafi. All rights reserved.
+//
+
+import Foundation
+
+struct Note {
+    let title:String
+    let content:String
+    let date:Date
+}
