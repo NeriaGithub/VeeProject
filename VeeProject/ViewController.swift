e2bab0e510e6ba84a19066696ab2ee3bf6fd5836
NeriaGithub <neria,jerafi@gmail.com>
2020-09-03 19:57:19 +0300

    first commit

---
+++ VeeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VeeProject
+//
+//  Created by Neria Jerafi on 03/09/2020.
+//  Copyright © 2020 Neria Jerafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
